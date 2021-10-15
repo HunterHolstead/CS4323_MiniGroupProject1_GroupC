3bba516cc256ca9232a02f0d0e0d7dd6996bbd52
ICook <isabellcook001@gmail.com>
2021-10-15 07:05:35 -0500

    MultiThreaded Client Server Implementation

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CS4323_MiniGroupProject1_GroupC
+
+Compile the server: "gcc -o server MiniProject_Cook_Isabell.c -lpthread"
+Compile the client: "gcc -o main main.c"
+Run server: ./server
+Run client: ./client
--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 //  Group C Driver File
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "MessagePassing.c"
+#include "clientHelper.h"
 
-#include "MessagePassing.h"
-
-// main function to run program
+/**
+ * Arguments:
+ *      void
+ * Description:
+ *      main driver file aka client that runs the program
+ * Return type:
+ *      void
+*/
 int main() {
-    menu(); // begin by displaying the menu to the user
+
+    int socket_client = clientStarter();
+
+    while (1)
+    {
+        /* code for program */
+        menu(); // begin by displaying the menu to the user
+    }
+
+    close(socket_client);
+
+    return 0;
 }
